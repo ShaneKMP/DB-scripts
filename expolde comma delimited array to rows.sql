530e011c3c8bb471f969f0f2b7a2eabff8fe1029
Shane <szhang@aspiration.com>
2019-02-05 12:35:29 -0800

    Update expolde comma delimited array to rows.sql

--- expolde comma delimited array to rows.sql
+++ expolde comma delimited array to rows.sql
@@ -5,12 +5,12 @@ SELECT evaluation_id,
   FROM rdl.alloy_evaluations_parsed
   --where evaluation_id = 18 --and extracted = '["32","10","RS","90"]'
   where extracted LIKE '[%]' and extracted != '[]'
-  )       
+  );       
                                                                                                                                                                                                                                                                                                                                  
-select * from tmp 
+select * from tmp; 
                                                                                                                                                                                       
 update tmp
-set extracted = replace(replace(replace(extracted, '"', ''), '[', ''), ']', '')                                                                                                                 
+set extracted = replace(replace(replace(extracted, '"', ''), '[', ''), ']', '');                                                                                                                 
                                                                                                                                                                                 
 create temporary table numbers as (
 select 1 as n union all
@@ -39,13 +39,13 @@ select 21 union all
 select 22 union all
 select 23 union all
 select 24 union all
-select 25)                                                     
+select 25)  ;                                                   
      
-select * from numbers
+select * from numbers;
                                                                                                                                                                              
 select tmp.*
   ,split_part(extracted, ',', n)
 from tmp
   cross join numbers
 where split_part(extracted, ',', n) is not null
-  and split_part(extracted, ',' ,n) != ''   
+  and split_part(extracted, ',' ,n) != '' ;  
